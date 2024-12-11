a2b7b22dac35128da26f5a92ad7e6352a0a83638
Marcus Barreto <marcusbarreto@outlook.com.br>
2024-12-11 11:33:34 -0300

    Add files via upload

---
+++ aula01py/atividade03.py
@@ -0,0 +1,4 @@
+itens = [42, "Python", 3.14, True, [1, 2, 3]]
+
+
+print(itens[2])
---
+++ aula01py/atividade04.py
@@ -0,0 +1,12 @@
+
+palestrantes = (
+    ("João Silva", "Inteligência Artificial", "Universidade X"),
+    ("Maria Oliveira", "Big Data", "Instituto Y"),
+    ("Carlos Souza", "Machine Learning", "Faculdade Z")
+)
+
+
+terceiro_palestrante = palestrantes[2]
+print(f"Nome: {terceiro_palestrante[0]}")
+print(f"Tema da palestra: {terceiro_palestrante[1]}")
+print(f"Instituição: {terceiro_palestrante[2]}")
